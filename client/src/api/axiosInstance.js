9d29acf99c84342e70dbaca521a2fab3bbce3f44
kmthein <kmthein96.mm@gmail.com>
2024-01-10 12:59:19 +0630

    profile err fixed

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,13 +2,22 @@ import axios from "axios";
 
 const getUpdateLocalStorage = () => {
     const updateToken = localStorage.getItem("token");
-    console.log(updateToken);
     return updateToken;
 }
 
 export const axiosInstance = axios.create({
     baseURL: `${import.meta.env.VITE_API}`,
-    headers: {
-        Authorization: `Bearer ${getUpdateLocalStorage()}`
-    }
 })
+
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = getUpdateLocalStorage();
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (err) => {
+        Promise.reject(err);
+    }
+)
